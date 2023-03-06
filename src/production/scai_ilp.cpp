022e621af1617d6720df5a4cef47088633b0fc3b
Nick Röder <nroeder@scai.fraunhofer.de>
2023-03-06 13:31:15 +0100

    AXS-2660: QA: WITH_ == 1

--- src/production/scai_ilp.cpp
+++ src/production/scai_ilp.cpp
@@ -10,7 +10,7 @@
 
 #include <windows.h> // for SetErrorMode
 
-#if WITH_MIMALLOC
+#if WITH_MIMALLOC == 1
 #pragma warning(push)
 #pragma warning(disable : 28251) // inconsistent annotation
 #pragma warning(disable : 4559)  // redefinition with __declspec(restrict)

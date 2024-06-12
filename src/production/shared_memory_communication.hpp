72db6532fb9c32be340d963699780f6e3bced56c
Jens Diewald <jens.diewald@scai.fraunhofer.de>
2024-06-12 12:37:14 +0200

    AXS-2833: Reduce indentation
    Remove namespace indentation and indentation of public/private keywords

--- src/production/shared_memory_communication.hpp
+++ src/production/shared_memory_communication.hpp
@@ -10,41 +10,43 @@
 
 namespace ilp_solver
 {
-    class CommunicationParent
-    {
-        public:
-            // Returns the name of the shared memory segment the data has been written to
-            std::string write_ilp_data(const ILPData& p_data);
-            void read_solution_data(ILPSolutionData* r_solution_data);
 
-        private:
-            std::unique_ptr<boost::interprocess::windows_shared_memory> d_shared_memory{};
-            std::unique_ptr<boost::interprocess::mapped_region>         d_mapped_region{};
+class CommunicationParent
+{
+public:
+    // Returns the name of the shared memory segment the data has been written to
+    std::string write_ilp_data(const ILPData& p_data);
+    void        read_solution_data(ILPSolutionData* r_solution_data);
 
-            // non-owned pointer; do not delete
-            void* d_address{};
-            void* d_result_address{};
+private:
+    std::unique_ptr<boost::interprocess::windows_shared_memory> d_shared_memory{};
+    std::unique_ptr<boost::interprocess::mapped_region>         d_mapped_region{};
 
-            std::string create_shared_memory(size_t p_size);
-    };
+    // non-owned pointer; do not delete
+    void* d_address{};
+    void* d_result_address{};
+
+    std::string create_shared_memory(size_t p_size);
+};
 
 
-    class CommunicationChild
-    {
-        public:
-            explicit CommunicationChild(const std::string& p_shared_memory_name);
-            CommunicationChild(const CommunicationChild&) = delete;
-            CommunicationChild(CommunicationChild&&) = delete;
+class CommunicationChild
+{
+public:
+    explicit CommunicationChild(const std::string& p_shared_memory_name);
+    CommunicationChild(const CommunicationChild&) = delete;
+    CommunicationChild(CommunicationChild&&)      = delete;
 
-            ILPDataView read_ilp_data();
-            void write_solution_data(const ILPSolutionData& p_solution_data);
+    ILPDataView read_ilp_data();
+    void        write_solution_data(const ILPSolutionData& p_solution_data);
 
-        private:
-            const boost::interprocess::windows_shared_memory d_shared_memory;
-            const boost::interprocess::mapped_region d_mapped_region;
+private:
+    const boost::interprocess::windows_shared_memory d_shared_memory;
+    const boost::interprocess::mapped_region         d_mapped_region;
 
-            // non-owned pointer; do not delete
-            void* const d_address;
-            void* d_result_address;
-    };
-}
+    // non-owned pointer; do not delete
+    void* const d_address;
+    void*       d_result_address;
+};
+
+} // namespace ilp_solver
